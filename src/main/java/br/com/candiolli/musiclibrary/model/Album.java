3a1e1773d923896c2ec685ab44597f0046c62be6
Silas Candiolli <silasca@gmail.com>
2019-02-14 22:46:55 -0200

    docs

--- src/main/java/br/com/candiolli/musiclibrary/model/Album.java
+++ src/main/java/br/com/candiolli/musiclibrary/model/Album.java
@@ -1,26 +1,12 @@
 package br.com.candiolli.musiclibrary.model;
 
-import br.com.candiolli.musiclibrary.util.JsonDateDeserializer;
-import br.com.candiolli.musiclibrary.util.JsonDateSerializer;
-import com.fasterxml.jackson.annotation.JsonFormat;
-import com.fasterxml.jackson.databind.annotation.JsonDeserialize;
-import com.fasterxml.jackson.databind.annotation.JsonSerialize;
 import lombok.Data;
 import org.hibernate.envers.AuditTable;
 import org.hibernate.envers.Audited;
 import org.springframework.data.rest.core.annotation.RestResource;
 
-import javax.persistence.CascadeType;
-import javax.persistence.Column;
-import javax.persistence.Entity;
-import javax.persistence.GeneratedValue;
-import javax.persistence.GenerationType;
-import javax.persistence.Id;
-import javax.persistence.JoinColumn;
-import javax.persistence.ManyToOne;
-import javax.persistence.Table;
+import javax.persistence.*;
 import java.io.Serializable;
-import java.time.LocalDateTime;
 
 @Audited
 @Entity
@@ -45,10 +31,7 @@ public class Album implements Serializable {
     @RestResource(exported = false)
     private Productor productor;
 
-    @JsonFormat(pattern = "YYYY-MM-dd")
     @Column(name = "dt_create", nullable = false)
-    @JsonSerialize(using = JsonDateSerializer.class)
-    @JsonDeserialize(using = JsonDateDeserializer.class)
-    private LocalDateTime dateCreate;
+    private String dateCreate;
 
 }
